5c0e09ba40af550cea7eb8164fd7db3391e6509d
Ксения Бахтиярова <kseniabakhtiarova@gmail.com>
2025-01-18 18:23:26 +0700

    commit

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
+import {createBrowserRouter, createHashRouter, createRoutesFromElements, Route} from "react-router-dom";
 import {RoutesPaths} from "./constants/RoutesPaths";
 import { Outlet } from "react-router-dom";
 
@@ -17,7 +17,7 @@ const Layout = () => {
     );
 };
 
-export const ROUTER = createBrowserRouter(
+export const ROUTER = createHashRouter(
     createRoutesFromElements(
         <Route path="/" element={<Layout />}>
             <Route index path={RoutesPaths.HOME} element={<Home />} />
